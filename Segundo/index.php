2152038b406d5ae43f1223b722f5285b2fd979ec
black961 <philip961@hotmail.com>
2016-02-15 23:11:08 -0500

    Fixing the ico deal

--- Segundo/index.php
+++ Segundo/index.php
@@ -17,19 +17,19 @@
 	  </tbody>
       <tbody>
         <tr align="center">
-          <td width="30%" height="30"><a href="index.php"><img src="home.png" width="30" height="30" alt=""/></a></td>
+          <td width="30%"><a href="index.php"><img src="Imagenes/home.png" width="30" height="30" alt=""/></a></td>
           <td width="70%" height="10"><h3><a href="index.php">Inicio</a></h3></td>
         </tr>
         <tr align="center">
-          <td width="30%"><a href="experiencia.php"><img src="experiencia.png" width="30" height="30" alt=""/></a></td>
+          <td width="30%"><a href="experiencia.php"><img src="Imagenes/experiencia.png" width="30" height="30" alt=""/></a></td>
           <td width="70%"><h3><a href="experiencia.php">Experiencia</a></h3></td>
         </tr>
         <tr align="center">
-          <td width="30%"><a href="formacion.php"><img src="conocimiento.png" width="30" height="30" alt=""/></a></td>
+          <td width="30%"><a href="formacion.php"><img src="Imagenes/conocimiento.png" width="30" height="30" alt=""/></a></td>
           <td width="70%"><h3><a href="formacion.php">Formación</a></h3></td>
         </tr>
         <tr align="center">
-          <td width="30%"><a href="referencias.php"><img src="referencias.png" width="30" height="30" alt=""/></a></td>
+          <td width="30%"><a href="referencias.php"><img src="Imagenes/referencias.png" width="30" height="30" alt=""/></a></td>
           <td width="70%"><h3><a href="referencias.php">Referencias</a></h3></td>
         </tr>
       </tbody>
